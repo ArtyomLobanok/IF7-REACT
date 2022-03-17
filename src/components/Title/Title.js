b7ce1f0a3ff4ff53a587305737d60c13e4c00b29
ArtyomLobanok <hejlenoctb3@gmail.com>
2022-03-17 11:16:58 +0300

    fix

---
+++ src/components/Title/Title.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Title.css';
+
+function Title(){
+    return (
+        <div className="section__header">
+            <h2 className="section__title">Homes guests loves</h2>
+        </div>
+    );
+}
+
+export default Title;
