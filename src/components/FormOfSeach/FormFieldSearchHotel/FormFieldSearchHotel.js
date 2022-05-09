b3004785b28958af3230d4181ac9fc64f2de81c7
ArtyomLobanok <hejlenoctb3@gmail.com>
2022-05-09 23:56:43 +0300

    deleted local state for input

--- src/components/FormOfSeach/FormFieldSearchHotel/FormFieldSearchHotel.js
+++ src/components/FormOfSeach/FormFieldSearchHotel/FormFieldSearchHotel.js
@@ -8,8 +8,8 @@ import {
 } from "../../Styled-components/HeaderForm";
 
 const FirstForm = () => {
-    const [values, setValues] = useState('');
     const [isActive, setIsActive] = useState(false);
+
     const handleFocus = (e) => {
         if (e.target.value === "") {
             disableField(e);
@@ -28,19 +28,18 @@ const FirstForm = () => {
     const handleChange = (value) => {
         dispatch(inputText(value))
     }
-    const searchText = useSelector(state => state.formReducer.search);
+    const inputValue = useSelector(state => state.formReducer.search);
+    console.log(inputValue)
     useEffect(
         () => {
-            if (searchText.length !== 0 && (window.scrollY > 2400)) {
+            if (inputValue.length !== 0 && (window.scrollY > 2400)) {
                 setIsActive(true)
                 window.scrollTo({
                     top: 0,
                     behavior: "smooth",
                 });
-            }
-            ;
-            setValues(searchText)
-        }, [searchText]);
+            };
+        }, [inputValue]);
 
     return (
         <StyledInputWrapper position='relative' maxWidth='467px'>
@@ -54,9 +53,8 @@ const FirstForm = () => {
                 name='search'
                 type='text'
                 id='searchName'
-                value={values}
+                value={inputValue}
                 onChange={(e) => {
-                    setValues(e.target.value)
                     handleChange(e.target.value)
                 }}
                 autoComplete='off'
